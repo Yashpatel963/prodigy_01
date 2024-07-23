ae9c24bbee347e4997eb21e633bd4b5d782d461c
Yashpatel963 <140719099+Yashpatel963@users.noreply.github.com>
2024-07-23 23:13:17 +0530

    Add files via upload

---
+++ PRODIGY_CS_01.py
@@ -0,0 +1,36 @@
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m',
+          'n','o','p','q','r','s','t','u','v','w','x','y','z']
+def encryption(plain_text,shift_key):#hello h=7
+    cipher_text=""
+    for char in plain_text:
+        if char in alphabet:
+            position=alphabet.index(char)
+            new_position=(position+shift_key)%26
+            cipher_text += alphabet[new_position]
+        else :
+            cipher_text += char
+    print(f"Here's is the text after encryption: {cipher_text}")
+def decryption(cipher_text,shift_key):
+    plain_text=""
+    for char in cipher_text:
+        if char in alphabet:
+            position=alphabet.index(char)
+            new_position=(position-shift_key)%26
+            plain_text += alphabet[new_position]
+        else:
+            plain_text+= char
+    print(f"Here's is the text after decryption: {plain_text}")
+
+wanna_end=False
+while not wanna_end:
+   what_to_do= input("Type 'encrypt' for encryption, type 'decrypt' for decryption: ")
+   text =input(" Type your message:\n")
+   shift=int(input("Enter shift key:\n"))
+   if what_to_do=="encrypt":
+    encryption(text,shift)
+   elif what_to_do=="decrypt":
+       decryption(text,shift)
+   play_again=input("Type 'yes' to continue, type 'no' to stop: ")
+   if play_again=='no':
+       wanna_end=True
+       print("Have a nice day! Bye....")
